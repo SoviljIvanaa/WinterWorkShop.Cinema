513319d313cb34da2a5f19fcd159dd0d346f76fd
Bogoljub Sojic <b.sojic@levi9.com>
2022-02-02 11:43:39 +0100

    added new fields to movie model

--- WinterWorkShop.Cinema.Data/Models/MovieModel.cs
+++ WinterWorkShop.Cinema.Data/Models/MovieModel.cs
@@ -3,6 +3,9 @@
     public class MovieModel
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string MovieName { get; set; }
+        public string LeadActor { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Plot { get; set; }
     }
 }
