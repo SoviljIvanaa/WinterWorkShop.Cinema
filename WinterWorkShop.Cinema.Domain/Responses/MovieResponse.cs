06373fc3e817e28d5b2843ec242146b5972f37b6
Bogoljub Sojic <b.sojic@levi9.com>
2022-02-02 15:08:34 +0100

    ading new properties to movie response

--- WinterWorkShop.Cinema.Domain/Responses/MovieResponse.cs
+++ WinterWorkShop.Cinema.Domain/Responses/MovieResponse.cs
@@ -3,6 +3,12 @@
     public class MovieResponse
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Description { get; set; }
+        public int TicketPrice { get; set; }
+        public string MovieName { get; set; }
+        public int MyProperty { get; set; }
+        public string LeadActor { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Plot { get; set; }
     }
 }
