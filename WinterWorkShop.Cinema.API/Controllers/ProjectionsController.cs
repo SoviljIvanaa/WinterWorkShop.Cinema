f90a4805158e76b817d43ab080a81af3963e5ac7
m.moskic-int <m.moskic-int@INTERNSHIP-ZR-2>
2023-03-06 15:23:19 +0100

    Task 2: Projections setup, get all projections endpoint added.

---
+++ WinterWorkShop.Cinema.API/Controllers/ProjectionsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WinterWorkShop.Cinema.Data.Repositories;
+using WinterWorkShop.Cinema.Domain.Responses;
+
+namespace WinterWorkShop.Cinema.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectionsController : BaseController
+    {
+        public readonly IProjectionsRepository _projectionsRepository;
+
+        public ProjectionsController(IProjectionsRepository projectionsRepository)
+        {
+            _projectionsRepository = projectionsRepository;
+        }
+
+        [HttpGet()]
+        public List<GetAllProjectionsResponse> GetProjections()
+        {
+            var projections = _projectionsRepository.GetAllProjections();
+
+            var result = new List<GetAllProjectionsResponse>();
+
+            foreach (var projection in projections)
+            {
+                result.Add(new GetAllProjectionsResponse
+                {
+                    Id = projection.Id,
+                    Name = projection.Name
+                });
+            }
+
+            return result;
+        }
+    }
+}
