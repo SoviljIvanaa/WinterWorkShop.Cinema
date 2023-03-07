e2a618de5429e891bb3da860fc8f2797aa2c5a8a
TheStankela <ognjenogistankovic2002@gmail.com>
2023-03-07 10:42:30 +0100

    Added not found custom exception

---
+++ WinterWorkShop.Cinema.Data/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace WinterWorkShop.Cinema.Data.Exceptions
+{
+	public class NotFoundException : Exception
+	{
+		public NotFoundException(int id) :base($"Object with ID {id} not found. ") { }
+        
+        
+        
+    }
+}
